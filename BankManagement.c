ca450ea5c7798fab2fb345af03f6eef16cd9c9d5
Dinoyan <dinoyan.g@hotmail.com>
2018-10-12 12:58:15 -0400

    fixed compile errors

--- BankManagement.c
+++ BankManagement.c
@@ -10,7 +10,8 @@ struct Customer {
 	char lastName[64];
 	int accNum;
 	int password;
-}
+};
+
 
 struct Admin {
 	int accType;
@@ -18,18 +19,34 @@ struct Admin {
 	char lastName[64];
 	int accNum;
 	int password;
-}
+};
+
 
 void admin(){
 	int logout = 0;
-
+	int authenticated;
+	//authenticated = authenticate();
+	if (authenticated == 1) {
+		while (logout == 0){
+			// admin menu.
+		}
+	}
 
 }
+
 
 void customer(){
 	int logout = 0;
+	int authenticated;
+	//authenticated = authenticate();
+	if (authenticated == 1) {
+		while (logout == 0){
+			// customer menu.
+		}
+	}
 
 }
+
 
 void createAccount(int userType){
 	char firstName[64];
@@ -37,15 +54,15 @@ void createAccount(int userType){
 	int password;
 	int accNum;
 	printf("Enter your first name: ");
-	scanf("%s", firstName);
+	scanf("%s", &firstName);
 	printf("Enter your last name: ");
-	scanf("%s", lastName);
-	printf("Enter a new account number: \n");
+	scanf("%s", &lastName);
+	printf("Enter your new account number: \n");
 
 	printf("Enter your new password\n");
-	scanf("%d", password);
+	scanf("%d", &password);
 
-	if (userType = 2){
+	if (userType == 2){
 		FILE *customerfile; 
       
     	// open file for writing 
@@ -55,7 +72,7 @@ void createAccount(int userType){
         	exit (1); 
     	} 
   
-   	 	struct Customer customer = {2, firstName, lastName, password}; 
+   	 	struct Customer customer = {2, firstName, lastName, 12, password}; 
       
     	// write struct to file 
     	fwrite (&customer, sizeof(struct Customer), 1, customerfile); 
@@ -69,7 +86,7 @@ void createAccount(int userType){
   
     	// close file 
     	fclose (customerfile); 
-	} else if(userType = SPECIALKEY) {
+	} else if(userType == SPECIALKEY) {
 		FILE *adminfile; 
       
     	// open file for writing 
@@ -98,38 +115,36 @@ void createAccount(int userType){
 
 }
 
+
 int authenticate(int userType, int accNum, int password){
 	int authenticated = 0;
-	int accNum;
-	int password;
 
 	if (userType == 1){
-		FILE *adminfile
+		FILE *adminfile;
 		struct Admin admin;
 
-		adminfile = fopen("admin.dat", 'r');
+		adminfile = fopen("admin.dat", "r");
 		if(adminfile == NULL) {
-			fprintf(stderr, "\nError opening file\n",);
+			fprintf(stderr, "\nError opening file\n");
 			exit(1);
 		}
 		while(fread(&admin, sizeof(struct Admin), 1, adminfile)){
-			if (admin.accNum == accNum && admi.password = password){
+			if ((admin.accNum == accNum) && (admin.password == password)){
 				authenticated = 1;
-			} else {
-				authenticated = 0;
-			}
+			} 
 		}
+
 	} else {
-		FILE *customerfile
+		FILE *customerfile;
 		struct Customer customer;
 
-		customerfile = fopen("customer.dat", 'r');
+		customerfile = fopen("customer.dat", "r");
 		if(customerfile == NULL) {
-			fprintf(stderr, "\nError opening file\n",);
+			fprintf(stderr, "\nError opening file\n");
 			exit(1);
 		}
 		while(fread(&customer, sizeof(struct Customer), 1, customerfile)){
-			if (customer.accNum == accNum && customer.password = password){
+			if ((customer.accNum == accNum) && (customer.password == password)){
 				authenticated = 1;
 			} else {
 				authenticated = 0;
@@ -141,6 +156,7 @@ int authenticate(int userType, int accNum, int password){
 	return authenticated;
 }
 
+
 int main(){
 	int userType;
 	int selected = 0;
@@ -148,9 +164,9 @@ int main(){
 	printf("Welcome to Dinoyan's Bank\n");
 	printf("Select your account type\n");
 	printf("1: Admin \n 2: Customer");
-	scanf("%d", userType);
+	scanf("%d", &userType);
 
-	while (selected = 0) {
+	while (selected == 0) {
 		if(userType == 1){
 			selected = 1;
 			system("CLS");
@@ -159,16 +175,15 @@ int main(){
 			selected = 1;
 			system("CLS");
 			customer();
-		} else if(selected = SPECIALKEY) {
+		} else if(selected == SPECIALKEY) {
 			system("CLS");
 			createAccount(SPECIALKEY);
 
-		{ else {
+		} else {
 			system("CLS");
 			printf("Please enter the correct type\n");
 			printf("1: Admin \n 2: Customer \n 3: New Customer");
-			scanf("%d", userType);
+			scanf("%d", &userType);
 		}
 	}
-	
 }
