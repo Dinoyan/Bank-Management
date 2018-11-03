3a18208bd5ff43becb0ba59180b004bbceb70759
Dinoyan <dinoyan.g98@gmail.com>
2018-11-03 14:24:16 -0400

    fuctions prototypes for customer

---
+++ customer.h
@@ -0,0 +1,14 @@
+#ifndef FUNCTIONS_H_INCLUDED
+#define FUNCTIONS_H_INCLUDED
+
+
+void customerInterface(int accNum);
+
+void getStatement(int accNum);
+
+void deposit(float amount);
+
+float withdraw(float amount);
+
+
+
